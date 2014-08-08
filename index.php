22c22d6138003f178b70e5f204b6e36bd01e77d6
Chris Holder <chris@see8ch.com>
2014-08-08 01:21:41 -0600

    location url change

--- index.php
+++ index.php
@@ -13,13 +13,11 @@
 	<h1>CHRIS HOLDER</h1>
 	<h4>User Friendly Design & Development</h4>
 
-	<a id="location" href="http://tinyurl.com/lnchs7w" target="_blank">
+	<a id="location" href="http://tinyurl.com/kymj9r8" target="_blank">
 		<img class="icon" src="images/location-icon.svg"/>
 		<p>Denver, CO</p>
 	</a>
 
-<!--	<div id="bkgd-map"></div>	-->
-<!--	<img class="icon mtn" src="images/mtn-icon.svg" />	-->
 	<svg class="mtn-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="8.2 13.2 104.1 42.5" enable-background="new 8.2 13.2 104.1 42.5" xml:space="preserve">
 		<polygon class="mtn-poly" fill="#414042" points="8.2,55.8 112.3,55.8 73.2,13.2 48.2,38.4 37.7,27.9 "/>
 	</svg>
